8789b882958cb817db9ee9ab749fd591baa28d25
FuracaoDoisMil <163349031+FuracaoDoisMil@users.noreply.github.com>
2026-05-01 16:23:58 -0300

    Update alteracoes.sql
    Mudei e adicionei atributos do banco

--- database/alteracoes.sql
+++ database/alteracoes.sql
@@ -1,1 +1,8 @@
+ALTER TABLE proprietarios
+ADD situacao ENUM('ativo', 'inativo') NOT NULL;
 
+ALTER TABLE carro
+modify situacao ENUM('ativo', 'inativo') NOT NULL;
+
+ALTER TABLE funcionarios
+modify situacao ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo';
