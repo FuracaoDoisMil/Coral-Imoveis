affb9afafb7a17ce4310d380eec00272c36b4f3d
FuracaoDoisMil <163349031+FuracaoDoisMil@users.noreply.github.com>
2026-05-06 10:35:52 -0300

    Update alteracoes.sql

--- database/alteracoes.sql
+++ database/alteracoes.sql
@@ -1,8 +1,8 @@
 ALTER TABLE proprietarios
-ADD situacao ENUM('ativo', 'inativo') NOT NULL;
+modify situacao ENUM('ativo', 'inativo') NOT NULL;
 
 ALTER TABLE carro
-modify situacao ENUM('ativo', 'inativo') NOT NULL;
+ADD situacao ENUM('disponivel', 'indisponivel') NOT NULL;
 
 ALTER TABLE funcionarios
 modify situacao ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo';
