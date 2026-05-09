ad822a51b1931e0f78c4bfa634639a3877431a27
FuracaoDoisMil <163349031+FuracaoDoisMil@users.noreply.github.com>
2026-05-09 14:40:02 -0300

    Delete database/alteracoes.sql

--- database/alteracoes.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE proprietarios
-modify situacao ENUM('ativo', 'inativo') NOT NULL;
-
-ALTER TABLE carro
-ADD situacao ENUM('disponivel', 'indisponivel') NOT NULL;
-
-ALTER TABLE funcionarios
-modify situacao ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo';
