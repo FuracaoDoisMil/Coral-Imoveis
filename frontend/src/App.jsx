b3a2f5cd22d085e3d328793766f004dea8cd080c
FuracaoDoisMil <163349031+FuracaoDoisMil@users.noreply.github.com>
2026-05-10 20:44:50 -0300

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <h1>Coral Imoveis</h1>
+
+      <p>Sistema Administrativo</p>
+
+      <button>Listar funcionarios</button>
+    </div>
+  )
+}
+
+export default App
