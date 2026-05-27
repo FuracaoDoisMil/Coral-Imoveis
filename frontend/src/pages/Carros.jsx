bac3ac44a1ecb6a88ba27bfb5ecca1b4e38ddefe
FuracaoDoisMil <163349031+FuracaoDoisMil@users.noreply.github.com>
2026-05-27 20:55:12 -0300

    Update Carros.jsx

--- frontend/src/pages/Carros.jsx
+++ frontend/src/pages/Carros.jsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 function Carros(){
+    const [carros, setCarros] = useState([])
+    
+    useEffect(() => {
+        fetch(`http://localhost:5000/carros`)
+
+        .then(resposta => {
+            console.log(resposta)
+            return resposta.json()
+        })
+
+        .then(dados => {
+            console.log(dados)
+            setCarros(dados)
+        })
+
+        .catch(erro => {
+            console.log("ERRO:", erro)
+        })
+    },[])
+
     return(
         <div>
-            <h2>Pagina Carros</h2>
+            <h1>Gerenciar Carros 🏎️</h1>
+            <div className="caixinhas_aba_carros">
+                <Link to="/carros/mostrar-carros" className="caixinha">
+                    <h3>Mostrar Carros</h3>
+                </Link>
+
+                <Link to="/carros/cadastrar-carros" className="caixinha">
+                    <h3>Cadastrar carros</h3>
+                </Link>
+            </div>
         </div>
     )
-
 }
-
 export default Carros
