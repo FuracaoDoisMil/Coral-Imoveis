1df416ddc599d69216d30575db918748f8d09524
FuracaoDoisMil <163349031+FuracaoDoisMil@users.noreply.github.com>
2026-05-29 13:15:45 -0300

    Update Visitas.jsx

--- frontend/src/pages/Visitas.jsx
+++ frontend/src/pages/Visitas.jsx
@@ -1,9 +1,42 @@
-function Visitas(){
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function Vendas(){
+    const [vendas, setVendas] = useState([])
+    
+    useEffect(() => {
+        fetch(`http://localhost:5000/vendas`)
+
+        .then(resposta => {
+            console.log(resposta)
+            return resposta.json()
+        })
+
+        .then(dados => {
+            console.log(dados)
+            setVendas(dados)
+        })
+
+        .catch(erro => {
+            console.log("ERRO:", erro)
+        })
+    },[])
+
     return(
         <div>
-            <h2>Pagina de Visitas</h2>
+            <h1>Gerenciar Vendas 💸</h1>
+            <div className="caixinhas_aba_visitas">
+                <Link to="/visitas/mostrar-visitas" className="caixinha">
+                    <h3>Mostrar Visitas</h3>
+                </Link>
+
+                <Link to="/visitas/cadastrar-visitas" className="caixinha">
+                    <h3>Cadastrar Visitas</h3>
+                </Link>
+
+
+            </div>
         </div>
     )
 }
-
-export default Visitas
+export default Vendas
