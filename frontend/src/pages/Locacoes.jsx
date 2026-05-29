2e4954d53492b968cc98271a182a6da3e23fc208
FuracaoDoisMil <163349031+FuracaoDoisMil@users.noreply.github.com>
2026-05-29 13:15:01 -0300

    Update Locacoes.jsx

--- frontend/src/pages/Locacoes.jsx
+++ frontend/src/pages/Locacoes.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 function Locacoes(){
+    const [locacoes, setLocacoes] = useState([])
+    
+    useEffect(() => {
+        fetch(`http://localhost:5000/locacoes`)
+
+        .then(resposta => {
+            console.log(resposta)
+            return resposta.json()
+        })
+
+        .then(dados => {
+            console.log(dados)
+            setLocacoes(dados)
+        })
+
+        .catch(erro => {
+            console.log("ERRO:", erro)
+        })
+    },[])
+
     return(
         <div>
-            <h2>Pagina de Locacoes</h2>
+            <h1>Gerenciar Locacoes 💸</h1>
+            <div className="caixinhas_aba_locacoes">
+                <Link to="/locacoes/mostrar-locacoes" className="caixinha">
+                    <h3>Mostrar Locações</h3>
+                </Link>
+            </div>
         </div>
     )
 }
-
 export default Locacoes
