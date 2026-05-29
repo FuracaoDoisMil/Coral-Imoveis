91a460d877457fab483fd43bed3c745a23134d8c
FuracaoDoisMil <163349031+FuracaoDoisMil@users.noreply.github.com>
2026-05-29 13:15:28 -0300

    Update Vendas.jsx

--- frontend/src/pages/Vendas.jsx
+++ frontend/src/pages/Vendas.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 function Vendas(){
+    const [vendas, setVendas] = useState([])
+    
+    useEffect(() => {
+        fetch(`http://localhost:5000/vendas`)
+
+        .then(resposta => {
+            console.log(resposta)
+            return resposta.json()
+        })
+
+        .then(dados => {
+            console.log(dados)
+            setVendas(dados)
+        })
+
+        .catch(erro => {
+            console.log("ERRO:", erro)
+        })
+    },[])
+
     return(
         <div>
-            <h2>Pagina de Vendas</h2>
+            <h1>Gerenciar Vendas 💸</h1>
+            <div className="caixinhas_aba_vendas">
+                <Link to="/vendas/mostrar-vendas" className="caixinha">
+                    <h3>Mostrar Vendas</h3>
+                </Link>
+            </div>
         </div>
     )
 }
-
 export default Vendas
