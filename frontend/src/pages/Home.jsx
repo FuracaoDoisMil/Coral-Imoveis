e76ef19ba149532b8065d2bc6bbd49d124f87930
FuracaoDoisMil <163349031+FuracaoDoisMil@users.noreply.github.com>
2026-06-25 21:04:46 -0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,6 @@ import CadastrarClientes from "./pages-Clientes/CadastrarClientes"
 import AtualizarClientes from "./pages-Clientes/AtualizarClientes"
 import DeletarClientes from "./pages-Clientes/DeletarClientes"
 
-
 import Proprietarios from "./Proprietarios"
 import MostrarProprietarios from "./pages-Proprietarios/MostrarProprietarios"
 import CadastrarProprietarios from "./pages-Proprietarios/CadastrarProprietarios"
@@ -28,7 +27,6 @@ import CadastrarImoveis from "./pages-Imoveis/CadastrarImoveis"
 import AtualizarImoveis from "./pages-Imoveis/AtualizarImoveis"
 import DeletarImoveis from "./pages-Imoveis/DeletarImoveis"
 
-
 import Vendas from "./Vendas"
 import MostrarVendas from "./pages-Vendas/MostrarVendas"
 import CadastrarVendas from "./pages-Vendas/CadastrarVendas"
@@ -39,17 +37,14 @@ import MostrarLocacoes from "./pages-Locacoes/MostrarLocacoes"
 import CadastrarLocacoes from "./pages-Locacoes/CadastrarLocacoes"
 import DeletarLocacoes from "./pages-Locacoes/DeletarLocacoes"
 
-
-
 import Visitas from "./Visitas"
 import MostrarVisitas from "./pages-Visitas/MostrarVisitas"
 import CadastrarVisitas from "./pages-Visitas/CadastrarVisitas"
 import AtualizarVisitas from "./pages-Visitas/AtualizarVisitas"
 import DeletarVisitas from "./pages-Visitas/DeletarVisitas"
 
-
 import Carros from "./Carros"
-import MostrarCarros from "./pages-Carros/MostrarCarros" 
+import MostrarCarros from "./pages-Carros/MostrarCarros"
 import CadastrarCarros from "./pages-Carros/CadastrarCarros"
 import AtualizarCarros from "./pages-Carros/AtualizarCarros"
 import DeletarCarros from "./pages-Carros/DeletarCarros"
@@ -63,6 +58,9 @@ import CadastrarContratos from "./pages-Contratos/CadastrarContratos"
 import DeletarContratos from "./pages-Contratos/DeletarContratos"
 
 function Home() {
+
+  const funcionario = JSON.parse(localStorage.getItem("funcionario"))
+  const tipo = funcionario?.tipo_funcionario
 
   return (
 
@@ -78,25 +76,14 @@ function Home() {
 
             <div>
 
-              <h2>
-                Sistema Administrativo
-              </h2>
+              <h2>Sistema Administrativo</h2>
 
               <div className="caixinhas">
 
-                <Link to="funcionarios" className="caixinha">
-                  <h3>Funcionários</h3>
-                  <p>Gerenciar equipe</p>
-                </Link>
-
+                {/* Todos veem clientes e imóveis */}
                 <Link to="clientes" className="caixinha">
                   <h3>Clientes</h3>
                   <p>Gerenciar clientes</p>
-                </Link>
-
-                <Link to="proprietarios" className="caixinha">
-                  <h3>Proprietários</h3>
-                  <p>Gerenciar proprietários</p>
                 </Link>
 
                 <Link to="imoveis" className="caixinha">
@@ -104,16 +91,22 @@ function Home() {
                   <p>Gerenciar imóveis</p>
                 </Link>
 
-                <Link to="vendas" className="caixinha">
-                  <h3>Vendas</h3>
-                  <p>Gerenciar vendas</p>
-                </Link>
+                {/* Corretor e Gerente */}
+                {(tipo === "Corretor" || tipo === "Gerente") && (
+                  <>
+                    <Link to="vendas" className="caixinha">
+                      <h3>Vendas</h3>
+                      <p>Gerenciar vendas</p>
+                    </Link>
 
-                <Link to="locacoes" className="caixinha">
-                  <h3>Locações</h3>
-                  <p>Gerenciar locações</p>
-                </Link>
+                    <Link to="locacoes" className="caixinha">
+                      <h3>Locações</h3>
+                      <p>Gerenciar locações</p>
+                    </Link>
+                  </>
+                )}
 
+                {/* Todos veem visitas e carros */}
                 <Link to="visitas" className="caixinha">
                   <h3>Visitas</h3>
                   <p>Gerenciar visitas</p>
@@ -124,10 +117,28 @@ function Home() {
                   <p>Gerenciar carros</p>
                 </Link>
 
-                <Link to="contratos" className="caixinha">
-                  <h3>Contratos</h3>
-                  <p>Gerenciar contratos</p>
-                </Link>
+                {/* Só Secretario e Gerente */}
+                {(tipo === "Secretario" || tipo === "Gerente") && (
+                  <>
+                    <Link to="proprietarios" className="caixinha">
+                      <h3>Proprietários</h3>
+                      <p>Gerenciar proprietários</p>
+                    </Link>
+
+                    <Link to="funcionarios" className="caixinha">
+                      <h3>Funcionários</h3>
+                      <p>Gerenciar equipe</p>
+                    </Link>
+                  </>
+                )}
+
+                {/* Só Gerente */}
+                {tipo === "Gerente" && (
+                  <Link to="contratos" className="caixinha">
+                    <h3>Contratos</h3>
+                    <p>Gerenciar contratos</p>
+                  </Link>
+                )}
 
               </div>
 
@@ -135,66 +146,60 @@ function Home() {
 
           } />
 
-          <Route path = "funcionarios" element={<Funcionarios />} />
-          <Route path = "funcionarios/cadastrar-funcionarios" element={<CadastrarFuncionarios/>}></Route>
-          <Route path = "funcionarios/mostrar-funcionarios" element={<MostrarFuncionarios/>}></Route>
-          <Route path = "funcionarios/atualizar-funcionarios/:id" element={<AtualizarFuncionarios/>}></Route>
-          <Route path = "funcionarios/deletar-funcionarios/:id" element={<DeletarFuncionario/>}></Route>
+          <Route path="funcionarios" element={<Funcionarios />} />
+          <Route path="funcionarios/cadastrar-funcionarios" element={<CadastrarFuncionarios />} />
+          <Route path="funcionarios/mostrar-funcionarios" element={<MostrarFuncionarios />} />
+          <Route path="funcionarios/atualizar-funcionarios/:id" element={<AtualizarFuncionarios />} />
+          <Route path="funcionarios/deletar-funcionarios/:id" element={<DeletarFuncionario />} />
 
           <Route path="clientes" element={<Clientes />} />
-          <Route path="clientes/mostrar-clientes" element={<MostrarClientes/>}></Route>
-          <Route path="clientes/cadastrar-clientes" element={<CadastrarClientes/>}></Route>
-          <Route path="clientes/atualizar-clientes/:id" element={<AtualizarClientes/>}></Route>
-          <Route path="clientes/deletar-clientes/:id" element={<DeletarClientes/>}></Route>
+          <Route path="clientes/mostrar-clientes" element={<MostrarClientes />} />
+          <Route path="clientes/cadastrar-clientes" element={<CadastrarClientes />} />
+          <Route path="clientes/atualizar-clientes/:id" element={<AtualizarClientes />} />
+          <Route path="clientes/deletar-clientes/:id" element={<DeletarClientes />} />
 
           <Route path="proprietarios" element={<Proprietarios />} />
-          <Route path="proprietarios/mostrar-proprietarios" element={<MostrarProprietarios/>}></Route>
-          <Route path="proprietarios/cadastrar-proprietarios" element={<CadastrarProprietarios/>}></Route>
-          <Route path="proprietarios/atualizar-proprietarios/:id" element={<AtualizarProprietarios/>}></Route>
-          <Route path="proprietarios/deletar-proprietarios/:id" element={<DeletarProprietarios/>}></Route>
+          <Route path="proprietarios/mostrar-proprietarios" element={<MostrarProprietarios />} />
+          <Route path="proprietarios/cadastrar-proprietarios" element={<CadastrarProprietarios />} />
+          <Route path="proprietarios/atualizar-proprietarios/:id" element={<AtualizarProprietarios />} />
+          <Route path="proprietarios/deletar-proprietarios/:id" element={<DeletarProprietarios />} />
 
           <Route path="imoveis" element={<Imoveis />} />
-          <Route path="imoveis/mostrar-imoveis" element={<MostrarImoveis/>}></Route>
-          <Route path="imoveis/detalhes-imoveis/:id" element={<DetalhesImoveis/>}></Route>
-          <Route path="imoveis/cadastrar-imoveis" element={<CadastrarImoveis/>}></Route>
-          <Route path="imoveis/atualizar-imoveis/:id" element={<AtualizarImoveis/>}></Route>
-          <Route path="imoveis/deletar-imoveis/:id" element={<DeletarImoveis/>}></Route>
-
+          <Route path="imoveis/mostrar-imoveis" element={<MostrarImoveis />} />
+          <Route path="imoveis/detalhes-imoveis/:id" element={<DetalhesImoveis />} />
+          <Route path="imoveis/cadastrar-imoveis" element={<CadastrarImoveis />} />
+          <Route path="imoveis/atualizar-imoveis/:id" element={<AtualizarImoveis />} />
+          <Route path="imoveis/deletar-imoveis/:id" element={<DeletarImoveis />} />
 
           <Route path="vendas" element={<Vendas />} />
-          <Route path="vendas/mostrar-vendas" element={<MostrarVendas/>}></Route>
-          <Route path="vendas/cadastrar-vendas/:id" element={<CadastrarVendas/>}></Route>
-          <Route path="vendas/deletar-vendas/:id" element={<DeletarVendas/>}></Route>
-
-
+          <Route path="vendas/mostrar-vendas" element={<MostrarVendas />} />
+          <Route path="vendas/cadastrar-vendas/:id" element={<CadastrarVendas />} />
+          <Route path="vendas/deletar-vendas/:id" element={<DeletarVendas />} />
 
           <Route path="locacoes" element={<Locacoes />} />
-          <Route path="locacoes/mostrar-locacoes" element={<MostrarLocacoes/>}></Route>
-          <Route path="locacoes/cadastrar-locacoes/:id" element={<CadastrarLocacoes/>}></Route>
-          <Route path="locacoes/deletar-locacoes/:id" element={<DeletarLocacoes/>}></Route>
-
+          <Route path="locacoes/mostrar-locacoes" element={<MostrarLocacoes />} />
+          <Route path="locacoes/cadastrar-locacoes/:id" element={<CadastrarLocacoes />} />
+          <Route path="locacoes/deletar-locacoes/:id" element={<DeletarLocacoes />} />
 
           <Route path="visitas" element={<Visitas />} />
-          <Route path="visitas/mostrar-visitas" element={<MostrarVisitas/>}></Route>
-          <Route path="visitas/cadastrar-visitas/:id" element={<CadastrarVisitas/>}></Route>
-          <Route path="visitas/atualizar-visitas/:id" element={<AtualizarVisitas/>}></Route>
-          <Route path="visitas/deletar-visitas/:id" element={<DeletarVisitas/>}></Route>
-
-
+          <Route path="visitas/mostrar-visitas" element={<MostrarVisitas />} />
+          <Route path="visitas/cadastrar-visitas/:id" element={<CadastrarVisitas />} />
+          <Route path="visitas/atualizar-visitas/:id" element={<AtualizarVisitas />} />
+          <Route path="visitas/deletar-visitas/:id" element={<DeletarVisitas />} />
 
           <Route path="carros" element={<Carros />} />
-          <Route path="carros/mostrar-carros" element={<MostrarCarros/>}></Route>
-          <Route path="carros/cadastrar-carros" element={<CadastrarCarros/>}></Route>
-          <Route path="carros/atualizar-carros/:id" element={<AtualizarCarros/>}></Route>
-          <Route path="carros/deletar-carros/:id" element={<DeletarCarros/>}></Route>
-          <Route path="carros/retorno-carro/:id" element={<AtualizarUsoCarro/>}></Route>
-          <Route path="carros/usar-carro/:id" element={<CadastrarUsoCarro/>}></Route>
+          <Route path="carros/mostrar-carros" element={<MostrarCarros />} />
+          <Route path="carros/cadastrar-carros" element={<CadastrarCarros />} />
+          <Route path="carros/atualizar-carros/:id" element={<AtualizarCarros />} />
+          <Route path="carros/deletar-carros/:id" element={<DeletarCarros />} />
+          <Route path="carros/retorno-carro/:id" element={<AtualizarUsoCarro />} />
+          <Route path="carros/usar-carro/:id" element={<CadastrarUsoCarro />} />
 
           <Route path="contratos" element={<Contratos />} />
-          <Route path="contratos/mostrar-contratos" element={<MostrarContratos/>}></Route>
-          <Route path="contratos/detalhes-contratos/:id" element={<DetalhesContratos/>}></Route>
-          <Route path="contratos/cadastrar-contratos" element={<CadastrarContratos/>}></Route>
-          <Route path="contratos/deletar-contratos/:id" element={<DeletarContratos/>}></Route>
+          <Route path="contratos/mostrar-contratos" element={<MostrarContratos />} />
+          <Route path="contratos/detalhes-contratos/:id" element={<DetalhesContratos />} />
+          <Route path="contratos/cadastrar-contratos" element={<CadastrarContratos />} />
+          <Route path="contratos/deletar-contratos/:id" element={<DeletarContratos />} />
 
         </Routes>
 
