b305138352a0b3bf93199c90117004b140d5f0fc
FuracaoDoisMil <163349031+FuracaoDoisMil@users.noreply.github.com>
2026-06-26 13:35:21 -0300

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,14 +2,11 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 function Login(){
-
     const navigate = useNavigate()
-
     const [email, setEmail] = useState("")
     const [senha, setSenha] = useState("")
 
     function fazerLogin(){
-
         if(!email || !senha){
             alert("Preencha email e senha!")
             return
@@ -22,60 +19,45 @@ function Login(){
             },
             body: JSON.stringify({ email, senha })
         })
-
         .then(resposta => resposta.json())
-
         .then(dados => {
-
             if(dados.erro){
                 alert("Email ou senha incorretos!")
                 return
             }
-
             localStorage.setItem("funcionario", JSON.stringify(dados))
-
             navigate("/admin")
-
         })
-
         .catch(erro => console.log("ERRO:", erro))
-
     }
 
     return(
-
         <div className="login-container">
-
             <h1>🪸 Coral Imóveis</h1>
-
             <div className="login-box">
-
                 <h2>Entrar no Sistema</h2>
-
                 <input
                     type="email"
                     placeholder="Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
-
                 <input
                     type="password"
                     placeholder="Senha"
                     value={senha}
                     onChange={(e) => setSenha(e.target.value)}
                 />
-
                 <button onClick={fazerLogin}>
                     Entrar
                 </button>
 
+                <button onClick={()=> navigate(`/`)}>
+                    Voltar
+                </button>
             </div>
-
         </div>
-
     )
-
 }
 
 export default Login
